d39529a9ef14caf4db9d119e1d8031426b99408a
nayem-1145 <nayem1145@gmail.com>
2024-12-07 22:00:05 +0600

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ $(function () {
   new WOW().init();
   ///////////////
   //header sticky
-  // $(window).on("scroll", function () {
-  //   var scrollBarPosition = $(this).scrollTop();
+  $(window).on("scroll", function () {
+    var scrollBarPosition = $(this).scrollTop();
 
-  //   if (scrollBarPosition > 150) {
-  //     $("header").addClass("sticky");
-  //   } else {
-  //     $("header").removeClass("sticky");
-  //   }
-  // });
+    if (scrollBarPosition > 150) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
   /////////////
   //6. Mobile Menu
   // Toggle the mobile menu and update mobile-menu-icon
@@ -41,5 +41,47 @@ $(function () {
     }
   });
 
-  /////////////////////////
+
+  /** category component */
+  $(".ct-single-item").each(function(){
+    $(this).hover(function(){
+      $(this).find("p").slideToggle();
+    });
+  })
+
+
+  var ct_item_slider = new Swiper('.ct-item-slider', {
+    loop: true,
+    slidesPerView: "4",
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1920: {
+        slidesPerView: 5,
+      },
+      2500: {
+        slidesPerView: 6,
+      },
+    },
+    
+  });
+
+
+
+  
 });
